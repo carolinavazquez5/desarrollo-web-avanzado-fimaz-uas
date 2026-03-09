2078aa52e9cbc971adc90b33d044c5a1d634e64d
carolinavazquez5 <vazquezcarolina342@gmail.com>
2026-03-09 06:46:44 -0700

    subiendo clases

---
+++ parcial-1-poo/practica-3/clases/Usuario.php
@@ -0,0 +1,27 @@
+<?php
+ class Usuario{
+    private $Nombre;
+    private $Correo;
+
+    function __construct($nombre, $correo)
+    {
+        if (!filter_var($correo, FILTER_VALIDATE_EMAIL)) {
+            throw new Exception("El correo no tiene un formato válido");
+        }
+
+        $this->Nombre=$nombre;
+        $this->Correo=$correo;
+    }
+
+    function getNombre()
+    {
+        return $this->Nombre;
+    }
+
+    function getCorreo()
+    {
+        return $this->Correo;
+    }
+
+ }
+ ?>
